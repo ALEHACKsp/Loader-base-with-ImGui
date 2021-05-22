1574e8f68e8325aaa7da3334f2b68ddfdac94159
Dylane <63970543+sDylane@users.noreply.github.com>
2021-05-22 12:54:22 +0300

    Add utilities files and inject files

---
+++ Source/Core/Inject/Inject.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef Inject_hh
+#define Inject_hh
+
+#include "../../Includes.hh"
+
+class CInject : public CSingleton<CInject> {
+public:
+
+private:
+
+};
+
+#endif // !Inject_hh
---
+++ Source/Utilities/Utilities.hh
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifndef Utilities_hh
+#define Utilities_hh
+
+#include "../Includes.hh"
+
+class CUtilities : public CSingleton<CUtilities> {
+public:
+
+private:
+
+};
+
+#endif // !Utilities_hh
